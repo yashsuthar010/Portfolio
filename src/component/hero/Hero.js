054760e5defc6f137a2ced6cd806adb6555cb64a
yash <yashsuthar010@gmail.com>
2022-08-21 15:20:09 +0530

    bg

--- src/component/hero/Hero.js
+++ src/component/hero/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./hero.css";
 import "./button.css";
 // import video3 from "../../assests/video3.mp4";
-import bg from "../../assests/bg.gif";
+import bg1 from "../../assests/bg1.gif";
 import Typewriter from "typewriter-effect";
 
 import "../../fonts/Centauri.woff";
@@ -40,7 +40,7 @@ const Hero = () => {
             </div>
           </div>
           <div className="hero_img">
-            <img src={bg}></img>
+            <img src={bg1}></img>
           </div>
           {/* <video src={video3} loop autoPlay muted></video> */}
         </div>
